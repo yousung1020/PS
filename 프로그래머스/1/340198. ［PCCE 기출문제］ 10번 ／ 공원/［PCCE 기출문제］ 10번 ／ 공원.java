7f348cf8386b42602cad71a5e22efe7807981227
Meteor <65354879+yousung1020@users.noreply.github.com>
2025-09-08 22:52:29 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 2.17 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,6 +1,65 @@
 import java.util.Arrays;
 
 class Solution {
+//     public int solution(int[] mats, String[][] park) {
+//         Integer[] matsI = new Integer[mats.length];
+//         boolean checkPlace = false;
+        
+//         for(int i = 0; i < mats.length; i++){
+//             matsI[i] = mats[i];
+//         }
+        
+//         Arrays.sort(matsI, (i1, i2) -> {
+//             return i2 - i1;
+//         });
+        
+//         for(int m: matsI){
+//             for(int r = 0; r <= park.length - m; r++){
+//                 for(int c = 0; c <= park[r].length - m; c++){
+//                     checkPlace = check_jari(m, r, c, park);
+                    
+//                     if(checkPlace){
+//                         return m;
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return -1;
+//     }
+    
+//     public boolean check_jari(int m, int r, int c, String[][] park){
+//         for(int i = r; i < r + m; i++){
+//             for(int j = c; j < c + m; j++){
+//                 if(!park[i][j].equals("-1")){
+//                     return false;
+//                 }
+//             }
+//         }
+        
+//         return true;
+//     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     public int solution(int[] mats, String[][] park) {
         Integer[] matsI = new Integer[mats.length];
         boolean checkPlace = false;
@@ -13,13 +72,13 @@ class Solution {
             return i2 - i1;
         });
         
-        for(int m: matsI){
-            for(int r = 0; r <= park.length - m; r++){
-                for(int c = 0; c <= park[r].length - m; c++){
-                    checkPlace = check_jari(m, r, c, park);
+        for(int mat: matsI){
+            for(int r = 0; r <= park.length - mat; r++){
+                for(int c = 0; c <= park[r].length - mat; c++){
+                    checkPlace = check_jari(mat, r, c, park);
                     
                     if(checkPlace){
-                        return m;
+                        return mat;
                     }
                 }
             }
@@ -28,9 +87,9 @@ class Solution {
         return -1;
     }
     
-    public boolean check_jari(int m, int r, int c, String[][] park){
-        for(int i = r; i < r + m; i++){
-            for(int j = c; j < c + m; j++){
+    public boolean check_jari(int mat,int r, int c, String[][] park){
+        for(int i = r; i < r + mat; i++){
+            for(int j = c; j < c + mat; j++){
                 if(!park[i][j].equals("-1")){
                     return false;
                 }
